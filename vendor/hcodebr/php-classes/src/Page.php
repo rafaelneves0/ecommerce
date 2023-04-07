d28767c171e39613021cefda95d83745f24292a3
Rafael Allan Brum Neves <rafael.neves0@hotmail.com>
2023-04-07 10:40:04 -0300

    add tpl admin

--- vendor/hcodebr/php-classes/src/Page.php
+++ vendor/hcodebr/php-classes/src/Page.php
@@ -8,17 +8,17 @@ class Page
 {
 
 	private $tpl;
-	private $options =[];
+	private $options = [];
 	private $defaults = [
 		"data"=>[]
 	];
-	public function __construct($opts = array())
+	public function __construct($opts = array(), $tpl_dir = "/views/")
 	{
 
 		$this->options = array_merge($this->defaults, $opts);
 
 		$config = array(
-			"tpl_dir"       => $_SERVER['DOCUMENT_ROOT']."/views/",
+			"tpl_dir"       => $_SERVER['DOCUMENT_ROOT'].$tpl_dir,
 			"cache_dir"     => $_SERVER['DOCUMENT_ROOT']."/views-cache/",
 			"debug"         => false
 		);
--- views-cache/index.dd9721a15f4a0eef5e16d5966271a993.rtpl.php
+++ views-cache/index.dd9721a15f4a0eef5e16d5966271a993.rtpl.php
@@ -88,7 +88,7 @@
                         <div class="product-carousel">
                             <div class="single-product">
                                 <div class="product-f-image">
-                                    <img src="img/product-1.jpg" alt="">
+                                    <img src="/res/site/img/product-1.jpg" alt="">
                                     <div class="product-hover">
                                         <a href="#" class="add-to-cart-link"><i class="fa fa-shopping-cart"></i> Add to cart</a>
                                         <a href="#" class="view-details-link"><i class="fa fa-link"></i> See details</a>

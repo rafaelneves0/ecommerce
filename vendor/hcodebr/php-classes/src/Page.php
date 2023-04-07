4e9383f8d70c27e487ec98d613d32e19d7453a74
Rafael Allan Brum Neves <rafael.neves0@hotmail.com>
2023-04-07 13:42:38 -0300

    tela de login

--- vendor/hcodebr/php-classes/src/Page.php
+++ vendor/hcodebr/php-classes/src/Page.php
@@ -10,6 +10,8 @@ class Page
 	private $tpl;
 	private $options = [];
 	private $defaults = [
+		"header"=>true,
+        "footer"=>true,
 		"data"=>[]
 	];
 	public function __construct($opts = array(), $tpl_dir = "/views/")
@@ -30,7 +32,7 @@ class Page
 
 		$this->setData($this->options["data"]);
 
-		$this->tpl->draw("header");
+		if($this->options["header"] === true) $this->tpl->draw("header");
 
 	}
 
@@ -54,7 +56,7 @@ class Page
 	public function __destruct()
 	{
 		
-		$this->tpl->draw("footer");
+		if($this->options["footer"] === true) $this->tpl->draw("footer");
 
 	}
 
